97258993ce785c108ec20e74c54e4b3d95597a4b
João Arthur Birkheuer de Cristo <joaobirkheuer@gmail.com>
2025-03-20 15:13:58 -0300

    Simple GET with query methods

--- src/main/java/com/mongoproject/workshopmongo/resources/PostResource.java
+++ src/main/java/com/mongoproject/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.mongoproject.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mongoproject.workshopmongo.domain.Post;
+import com.mongoproject.workshopmongo.resources.util.URL;
 import com.mongoproject.workshopmongo.services.PostService;
 
 @RestController
@@ -21,6 +25,16 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id){
 		Post p = ps.findById(id);
 		return ResponseEntity.ok().body(p);
+	}
+	
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle
+	(@RequestParam(value = "text", defaultValue = "")
+	String txt){
+		
+		txt = URL.decodeParameter(txt);
+		List<Post> list = ps.findByTitle(txt);
+		return ResponseEntity.ok().body(list);
 	}
 	
 }
--- src/main/java/com/mongoproject/workshopmongo/services/PostService.java
+++ src/main/java/com/mongoproject/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.mongoproject.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,11 @@ public class PostService {
 	public Post findById(String id) {
 	    Optional<Post> obj = pr.findById(id);
 	    return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	/// ENCODE VALUE IN THE URL
+	/// CREATING UTILITARY CLASS
+	public List<Post> findByTitle(String title){
+		return pr.findByTitleContaining(title);
 	}
 }
